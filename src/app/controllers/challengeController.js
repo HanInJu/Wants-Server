874d2024d48800d05766c588731f50a5ba695822
ChaeMin0215 <chaem0215@naver.com>
2021-03-18 14:08:11 +0900

    challenge 수정

--- src/app/controllers/challengeController.js
+++ src/app/controllers/challengeController.js
@@ -49,7 +49,8 @@ exports.postchallenge = async function (req, res) {
       });
     // 겹치는 날짜 확인
     const calendarYNRows = await challengeDao.calendarYN(jwt, expriodAt);
-    if (calendarYNRows)
+    console.log(calendarYNRows);
+    if (calendarYNRows.length > 0)
       return res.json({
         isSuccess: false,
         code: 2122,
