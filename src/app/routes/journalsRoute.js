0002828f0f29b01664a4f861f597402fc74f2133
ChaeMin0215 <chaem0215@naver.com>
2021-03-19 18:02:40 +0900

    일지와 리뷰 분리

--- src/app/routes/journalsRoute.js
+++ src/app/routes/journalsRoute.js
@@ -1,17 +1,10 @@
 module.exports = function (app) {
-  const review = require("../controllers/reviewController");
+  const journals = require("../controllers/journalsController");
   const jwtMiddleware = require("../../../config/jwtMiddleware");
 
-  app.get("/review/", jwtMiddleware, review.getReview);
-  app.route("/review").post(jwtMiddleware, review.postReview);
-  //app.patch('/review', jwtMiddleware, review.reviseReview);
-  //app.route('/review').delete(jwtMiddleware, review.deleteReview);
-
-  //app.route('/report-review').post(jwtMiddleware, review.reportReview);
-
   //app.get("/journals", jwtMiddleware, challenge.getjournals); // 일지 조회
-  app.post("/journals", jwtMiddleware, review.postjournals); // 일지 추가
-  app.patch("/journals", jwtMiddleware, review.patchjournals); // 일지 수정
+  app.post("/journals", jwtMiddleware, journals.postjournals); // 일지 추가
+  app.patch("/journals", jwtMiddleware, journals.patchjournals); // 일지 수정
   //app.get("/journals/:journalsId", jwtMiddleware, challenge.getpatchjournals); // 일지 수정위한 조회
   //app.delete("/journals", jwtMiddleware, challenge.deletejournals); // 일지 삭제
 };
