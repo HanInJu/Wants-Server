52c333eca52ff1114f20985616d5a94a1658d299
ChaeMin0215 <chaem0215@naver.com>
2021-03-19 20:16:51 +0900

    일지 CUD API

--- src/app/controllers/journalsController.js
+++ src/app/controllers/journalsController.js
@@ -8,101 +8,6 @@ const userDao = require("../dao/userDao");
 /*
  * API 기 능 : 일지 추가
  */
-exports.patchjournals = async function (req, res) {
-  try {
-    var jwt = req.verifiedToken.id;
-
-    const userRows = await userDao.getuser(jwt);
-    if (userRows[0] === undefined)
-      return res.json({
-        isSuccess: false,
-        code: 4020,
-        message: "가입되어있지 않은 유저입니다.",
-      });
-
-    const { text, journalImageURL, open, page, percent } = req.body;
-
-    if (
-      time.length === 0 ||
-      time === undefined ||
-      text.length === 0 ||
-      text === undefined ||
-      open.length === 0 ||
-      open === undefined ||
-      page.length === 0 ||
-      page === undefined ||
-      percent.length === 0 ||
-      percent === undefined ||
-      goalBookId.length === 0 ||
-      goalBookId === undefined
-    )
-      return res.json({
-        isSuccess: false,
-        code: 2100,
-        message: "입력을 해주세요.",
-      });
-    console.log(goalBookId);
-    const whatIsYourName = await reviewDao.whatIsYourName(jwt);
-    if (whatIsYourName[0].name === "Reader") {
-      return res.json({
-        isSuccess: false,
-        code: 3001,
-        message: "닉네임을 설정해주세요.",
-      });
-    }
-
-    const goalId1 = await reviewDao.getgoalBookId(goalBookId);
-    const goalId = goalId1[0].GoalId;
-
-    if (goalId.length == 0)
-      return res.json({
-        isSuccess: true,
-        code: 2225,
-        message: "읽은 책이 없습니다.",
-      });
-
-    const postjournalsRows = await reviewDao.postjournals(
-      time,
-      page,
-      percent,
-      goalBookId,
-      goalId,
-      jwt
-    );
-
-    if (postjournalsRows.affectedRows === 1) {
-      const challengeId = postjournalsRows.insertId;
-      const postjournals2Rows = await reviewDao.postjournals2(
-        challengeId,
-        text,
-        journalImageURL,
-        open
-      );
-      if (postjournals2Rows.affectedRows === 1)
-        return res.json({
-          isSuccess: true,
-          code: 1000,
-          message: "일지 작성 성공",
-        });
-      else if (postjournals2Rows.affectedRows === 0)
-        return res.json({
-          isSuccess: true,
-          code: 4000,
-          message: "일지 작성 실패",
-        });
-    } else
-      return res.json({
-        isSuccess: false,
-        code: 4000,
-        message: "일지 작성 실패",
-      });
-  } catch (err) {
-    logger.error(`App - SignUp Query error\n: ${err.message}`);
-    return res.status(500).send(`Error: ${err.message}`);
-  }
-};
-
-//일지수정
 exports.postjournals = async function (req, res) {
   try {
     var jwt = req.verifiedToken.id;
@@ -154,17 +59,17 @@ exports.postjournals = async function (req, res) {
       });
     }
 
-    const goalId1 = await reviewDao.getgoalBookId(goalBookId);
+    const goalId1 = await journalsDao.getgoalBookId(goalBookId);
     const goalId = goalId1[0].GoalId;
 
     if (goalId.length == 0)
       return res.json({
         isSuccess: true,
         code: 2225,
-        message: "읽은 책이 없습니다.",
+        message: "목표가 없습니다.",
       });
 
-    const postjournalsRows = await reviewDao.postjournals(
+    const postjournalsRows = await journalsDao.postjournals(
       time,
       page,
       percent,
@@ -175,7 +80,7 @@ exports.postjournals = async function (req, res) {
 
     if (postjournalsRows.affectedRows === 1) {
       const challengeId = postjournalsRows.insertId;
-      const postjournals2Rows = await reviewDao.postjournals2(
+      const postjournals2Rows = await journalsDao.postjournals2(
         challengeId,
         text,
         journalImageURL,
@@ -198,6 +103,114 @@ exports.postjournals = async function (req, res) {
         isSuccess: false,
         code: 4000,
         message: "일지 작성 실패",
+      });
+  } catch (err) {
+    logger.error(`App - SignUp Query error\n: ${err.message}`);
+    return res.status(500).send(`Error: ${err.message}`);
+  }
+};
+
+//일지수정
+exports.patchjournals = async function (req, res) {
+  try {
+    var jwt = req.verifiedToken.id;
+
+    const userRows = await userDao.getuser(jwt);
+    if (userRows[0] === undefined)
+      return res.json({
+        isSuccess: false,
+        code: 4020,
+        message: "가입되어있지 않은 유저입니다.",
+      });
+
+    const { journalId, text, journalImageURL, open } = req.body;
+
+    if (
+      journalId.length === 0 ||
+      journalId === undefined ||
+      text.length === 0 ||
+      text === undefined ||
+      open.length === 0 ||
+      open === undefined
+    )
+      return res.json({
+        isSuccess: false,
+        code: 2100,
+        message: "입력을 해주세요.",
+      });
+
+    const patchjournalsRows = await journalsDao.patchjournals(
+      journalId,
+      text,
+      journalImageURL,
+      open
+    );
+
+    if (patchjournalsRows.changedRows === 1) {
+      return res.json({
+        isSuccess: true,
+        code: 1000,
+        message: "일지 수정 성공",
+      });
+    } else if (patchjournalsRows.changedRows === 0) {
+      return res.json({
+        isSuccess: false,
+        code: 2226,
+        message: "수정할 내용이 없습니다.",
+      });
+    } else
+      return res.json({
+        isSuccess: false,
+        code: 4000,
+        message: "일지 수정 실패",
+      });
+  } catch (err) {
+    logger.error(`App - SignUp Query error\n: ${err.message}`);
+    return res.status(500).send(`Error: ${err.message}`);
+  }
+};
+
+//일지삭제
+exports.deletejournals = async function (req, res) {
+  try {
+    var jwt = req.verifiedToken.id;
+
+    const userRows = await userDao.getuser(jwt);
+    if (userRows[0] === undefined)
+      return res.json({
+        isSuccess: false,
+        code: 4020,
+        message: "가입되어있지 않은 유저입니다.",
+      });
+
+    const { journalId } = req.body;
+    console.log(journalId);
+    if (journalId.length === 0 || journalId === undefined)
+      return res.json({
+        isSuccess: false,
+        code: 2100,
+        message: "입력을 해주세요.",
+      });
+
+    const deletejournalsRows = await journalsDao.deletejournals(journalId);
+    console.log(deletejournalsRows);
+    if (deletejournalsRows.affectedRows === 1) {
+      return res.json({
+        isSuccess: true,
+        code: 1000,
+        message: "일지 삭제 성공",
+      });
+    } else if (deletejournalsRows.affectedRows === 0) {
+      return res.json({
+        isSuccess: false,
+        code: 2227,
+        message: "삭제할 일지가 없습니다.",
+      });
+    } else
+      return res.json({
+        isSuccess: false,
+        code: 4000,
+        message: "일지 삭제 실패",
       });
   } catch (err) {
     logger.error(`App - SignUp Query error\n: ${err.message}`);
--- src/app/dao/journalsDao.js
+++ src/app/dao/journalsDao.js
@@ -1,123 +1,5 @@
 const { pool } = require("../../../config/database");
 
-async function whatIsYourName(userId) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const nameQuery = `
-                 SELECT name FROM User WHERE userId = ?;
-                 `;
-
-  const nameParams = [userId];
-  const [nameRows] = await connection.query(nameQuery, nameParams);
-
-  connection.release();
-  return nameRows;
-}
-
-async function isPosted(bookId, text) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const postedReviewQuery = `
-                 SELECT EXISTS(SELECT * FROM Review WHERE bookId = ? AND text = ?) as exist;
-                 `;
-
-  const postedReviewParams = [bookId, text];
-  const [exist] = await connection.query(postedReviewQuery, postedReviewParams);
-
-  connection.release();
-  return exist;
-}
-
-async function isValidBookId(bookId) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const validQuery = `
-                     SELECT EXISTS(SELECT * FROM Book WHERE bookId = ?) as exist;
-                     `;
-
-  const validParams = [bookId];
-  const [validRows] = await connection.query(validQuery, validParams);
-
-  connection.release();
-  return validRows;
-}
-
-async function postReview(reviewParams) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const reviewQuery = `
-                 INSERT INTO Review(bookId, userId, star, text, isPublic)
-                 VALUES (?,?,?,?,?)
-                 `;
-
-  const [reviewRows] = await connection.query(reviewQuery, reviewParams);
-
-  connection.release();
-  return reviewRows;
-}
-
-async function isValidReviewId(reviewId) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const validReviewQuery = `
-    SELECT EXISTS(SELECT * FROM Review WHERE reviewId = ? AND status = 'OK') as exist;
-                 `;
-  const validReviewParam = [reviewId];
-  const [validReviewRow] = await connection.query(
-    validReviewQuery,
-    validReviewParam
-  );
-
-  connection.release();
-  return validReviewRow;
-}
-
-async function isAuthorizedUser(reviewId) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const validReviewQuery = `
-                 SELECT userId FROM Review WHERE reviewId = ?;
-                 `;
-  const validReviewParam = [reviewId];
-  const [validReviewRow] = await connection.query(
-    validReviewQuery,
-    validReviewParam
-  );
-
-  connection.release();
-  return validReviewRow;
-}
-
-async function isDuplicatedText(text) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const duplicatedTextQuery = `
-    SELECT EXISTS(SELECT * FROM Review WHERE text = ?) as exist;
-                      `;
-
-  const [duplicatedTextRow] = await connection.query(duplicatedTextQuery, text);
-
-  connection.release();
-  return duplicatedTextRow;
-}
-
-async function reviseReview(reviseReviewParams) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const publicQuery = `
-                      UPDATE Review SET star = ?, text = ?, isPublic = ? WHERE reviewId = ?;
-                      `;
-
-  const [publicRow] = await connection.query(publicQuery, reviseReviewParams);
-
-  connection.release();
-  return publicRow;
-}
-
-async function deleteReview(reviewId) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const deleteQuery = `
-                       UPDATE Review SET status = 'DELETE' WHERE reviewId = ?;
-                      `;
-
-  const [deleteRow] = await connection.query(deleteQuery, reviewId);
-
-  connection.release();
-  return deleteRow;
-}
-
 //////////////////////////////////////////일지추가//////////////////////////////////////////////
 async function postjournals(time, page, percent, goalBookId, goalId, jwt) {
   const connection = await pool.getConnection(async (conn) => conn);
@@ -138,7 +20,7 @@ async function postjournals2(challengeId, text, journalImageURL, open) {
   connection.release();
   return calendarYNRows;
 }
-//////////////////////////////////////////일지추가2//////////////////////////////////////////////
+//////////////////////////////////////////목표인덱스 찾기//////////////////////////////////////////////
 async function getgoalBookId(goalBookId) {
   const connection = await pool.getConnection(async (conn) => conn);
   const calendarYNQuery = `
@@ -149,18 +31,32 @@ async function getgoalBookId(goalBookId) {
   connection.release();
   return calendarYNRows;
 }
-
+//////////////////////////////////////////일지 수정//////////////////////////////////////////////
+async function patchjournals(journalId, text, journalImageURL, open) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const calendarYNQuery = `
+  UPDATE Reading_journal 
+  SET text = '${text}', journalImageURL = '${journalImageURL}', open = '${open}'
+  WHERE journalId = ${journalId}`;
+  const [calendarYNRows] = await connection.query(calendarYNQuery);
+  connection.release();
+  return calendarYNRows;
+}
+//////////////////////////////////////////일지 삭제//////////////////////////////////////////////
+async function deletejournals(journalId) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const calendarYNQuery = `
+  DELETE
+  FROM Reading_journal
+  WHERE journalId = ${journalId}`;
+  const [calendarYNRows] = await connection.query(calendarYNQuery);
+  connection.release();
+  return calendarYNRows;
+}
 module.exports = {
-  postReview,
-  whatIsYourName,
-  isPosted,
-  isValidBookId,
-  isAuthorizedUser,
-  isDuplicatedText,
-  reviseReview,
-  isValidReviewId,
-  deleteReview,
   postjournals,
   postjournals2,
   getgoalBookId,
+  patchjournals,
+  deletejournals,
 };
