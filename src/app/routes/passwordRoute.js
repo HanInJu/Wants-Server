b809bb93450075751e4f56f64f2bd3b50ebb4a74
HanInJu <haninju95@gmail.com>
2021-03-27 22:22:14 +0900

    CHANGE PW API 완성

--- src/app/routes/passwordRoute.js
+++ src/app/routes/passwordRoute.js
@@ -2,8 +2,7 @@ module.exports = function(app){
     const pw = require('../controllers/passwordController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
-    // app.route('/signUp').post(user.signUp);
-    // app.route('/signIn').post(user.signIn);
-    //
-    app.patch('/email', pw.sendEmail);
+    //app.post('/email', pw.sendEmail);
+    app.route('/email').post(pw.sendEmail);
+    app.patch('/reset', jwtMiddleware, pw.resetPW);
 };
