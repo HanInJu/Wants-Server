6f0c8c38968ca6346f5a8d654a69c0f2eb34f1fd
HanInJu <haninju95@gmail.com>
2021-03-30 21:27:03 +0900

    prod 서버용 파일 업로드

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("./config/express");
 const { logger } = require("./config/winston");
 
-const port = 4000;
+// const port = 4000; //dev 서버용
+const port = 5000; //prod 서버
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
