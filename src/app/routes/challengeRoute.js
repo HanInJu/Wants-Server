643a8b79d8807b74107e72b4b27b9b9b499c6363
ChaeMin0215 <chaem0215@naver.com>
2021-05-06 15:03:03 +0900

    시간만 변경 API

--- src/app/routes/challengeRoute.js
+++ src/app/routes/challengeRoute.js
@@ -13,6 +13,7 @@ module.exports = function (app) {
     challenge.deletechallengeBook
   ); // 챌린지 책 삭제
   app.patch("/challenge", jwtMiddleware, challenge.patchchallenge); // 목표 변경
+  app.patch("/challenge/time", jwtMiddleware, challenge.patchchallengeTime); // 시간만 변경
 
   app.get("/challenge/:goalBookId/times", jwtMiddleware, challenge.getbookTime); // 오늘 읽은 책의 총 시간
 
